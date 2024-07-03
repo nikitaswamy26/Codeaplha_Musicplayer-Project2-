467ff6598f796d890219b8b26a30ea83c5d42db5
Nikita Swamy <151628180+nikitaswamy26@users.noreply.github.com>
2024-07-03 17:39:33 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,26 @@
+let trackList = [
+  {
+    name: "A New Beginning",
+    path: "./bensound-anewbeginning.mp3",
+    img: "./img1.jpg",
+    singer: "Bensound",
+  },
+  {
+    name: "Hey",
+    path: "./bensound-hey.mp3",
+    img: "./img2.jpg",
+    singer: "Bensound",
+  },
+  {
+    name: "Little Idea",
+    path: "./bensound-littleidea.mp3",
+    img: "./img3.jpg",
+    singer: "Bensound",
+  },
+  {
+    name: "Sunny",
+    path: "./bensound-sunny.mp3",
+    img: "./img4.jpg",
+    singer: "Bensound",
+  },
+];
